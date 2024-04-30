0e275cdc58d4d02872624e5e520efa55fe693b1a
Gustavo Todeschini <gutodeschinii@outlook.com>
2024-04-29 23:42:32 -0300

    experimentos com novos métodos para arrays

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const pessoa = ["Dimitri", "Teixeira", 30, "Professor"]; // array
+// const pessoa = ["Dimitri", "Teixeira", 30, "Professor"]; // array
 
 // pessoa.push("Brasileiro"); -- método de adicionar mais um elemento no array
 //pessoa[pessoa.length] = "Casado"; // Outro exemplo de como adicionar mais um elemento no array
@@ -11,5 +11,13 @@ const pessoa = ["Dimitri", "Teixeira", 30, "Professor"]; // array
 
 //pessoa.unshift("Gustavão");//Isso serve para adicionar um item ao array, mas na primeira posição
 
-delete pessoa[0]; //isso vai deletar a primeira posição do array, mas não vai redefinir a sequência, vai deixar a posição 0 no caso como undefined.
-document.getElementById("teste").innerHTML = pessoa.join(" <br> "); // o método join permite adicionar informações entre os elementos do array que foram substituído no element html
+//delete pessoa[0]; //isso vai deletar a primeira posição do array, mas não vai redefinir a sequência, vai deixar a posição 0 no caso como undefined.
+// pessoa.splice(1, 0, "Item adicionado 1", "item adicionado 2");
+
+const lista1 = ["Arroz", "feijão", "leite", "macarrão"]; //array
+const lista2 = ["Suco", "refrigerante", "carne"];
+const lista3 = ["Salgadinho"]
+
+const superLista = lista1.concat(lista2, lista3);
+
+document.getElementById("teste").innerHTML = superLista;
